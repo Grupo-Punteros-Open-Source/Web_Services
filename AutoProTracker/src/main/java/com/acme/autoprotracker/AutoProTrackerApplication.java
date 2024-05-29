4ca0dde13df41b82d3eda1e8c321798d75096127
Christian Matos <165172032+FerKlox@users.noreply.github.com>
2024-05-28 22:25:21 -0500

    feat: added directory shared

---
+++ AutoProTracker/src/main/java/com/acme/autoprotracker/AutoProTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.acme.autoprotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoProTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoProTrackerApplication.class, args);
+    }
+
+}
---
+++ AutoProTracker/src/main/java/com/acme/autoprotracker/shared/interfaces/rest/resources/MessageResource.java
@@ -0,0 +1,4 @@
+package com.acme.autoprotracker.shared.interfaces.rest.resources;
+
+public record MessageResource() {
+}
